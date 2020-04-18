4b290c7f7cdbeb7387795f9fb0594277441127e6
aal-brown <adlai.brown@gmail.com>
2020-04-18 13:14:20 -0600

    Adding more components to the game, fleshing out the cards object. Have all of the card images now imported and resized.

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,19 @@ import "./CardsSelect"
 import './App.css';
 import CardsSelect from './CardsSelect';
 import { createGameCardsArray } from './card-functions'
+import yoshi from "./images/yoshi-rs.png"
+import bobOmb from "./images/bob-omb-rs.png"
+import bowser from "./images/bowser-rs.png"
+import goomba from "./images/goomba-rs.png"
+import koopa from "./images/koopa-rs.png"
+import luigi from "./images/luigi-rs.png"
+import mario from "./images/mario-rs.png"
+import mushroom from "./images/mushroom-rs.png"
+import peach from "./images/peach-rs.png"
+import piranhaPlant from "./images/piranha-plant-rs.png"
+import questionBlock from "./images/question-block.png"
+import star from "./images/star-rs.png"
+import toad from "./images/toad-rs.png"
 
 function App() {
   let cardsArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"];
@@ -18,7 +31,21 @@ function App() {
         />
       )} 
       { view === 1 && (
-        <div>test</div>
+        <div>
+          <img src={yoshi} alt="Error"/>
+          <img src={bobOmb} alt="Error"/>
+          <img src={bowser} alt="Error"/>
+          <img src={goomba} alt="Error"/>
+          <img src={koopa} alt="Error"/>
+          <img src={luigi} alt="Error"/>
+          <img src={mario} alt="Error"/>
+          <img src={mushroom} alt="Error"/>
+          <img src={peach} alt="Error"/>
+          <img src={piranhaPlant} alt="Error"/>
+          <img src={questionBlock} alt="Error"/>
+          <img src={star} alt="Error"/>
+          <img src={toad} alt="Error"/>
+        </div>
       )}
     </div>
   )
---
+++ src/components/cardObjects.js
@@ -0,0 +1,43 @@
+import React from "react"
+import yoshi from "./images/yoshi-rs.png"
+import bobOmb from "./images/bob-omb-rs.png"
+import bowser from "./images/bowser-rs.png"
+import goomba from "./images/goomba-rs.png"
+import koopa from "./images/koopa-rs.png"
+import luigi from "./images/luigi-rs.png"
+import mario from "./images/mario-rs.png"
+import mushroom from "./images/mushroom-rs.png"
+import peach from "./images/peach-rs.png"
+import piranhaPlant from "./images/piranha-plant-rs.png"
+import questionBlock from "./images/question-block.png"
+import star from "./images/star-rs.png"
+import toad from "./images/toad-rs.png"
+
+
+//Making the cards instances of a class will allow me to add methods if necessary that will be applied equally to all. It will also make it simpler to create the objects.
+
+let imageArray = [bobOmb, bowser, goomba, koopa, luigi, mario, mushroom, peach, piranhaPlant, star, toad, yoshi];
+
+class Card {
+  constructor(id, front) {
+    this.id = id
+    this.back = questionBlock
+    this.front = front
+  }
+}
+
+let createCardsArr = function(imageArray) {
+  let cardsArray = [];
+
+  for (let i = 0; i < imageArray.length; i ++) {
+    cardsArray.push(new Card(i+1, imageArray[i]))
+  }
+
+  return cardsArray;
+}
+
+let cardsArray = createCardsArr(imageArray)
+
+console.log(cardsArray)
+
+
